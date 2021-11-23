f0836a837b085a404d2e4f47fbe5435726009c71
LunarStone292 <93814414+LunarStone292@users.noreply.github.com>
2021-11-23 07:58:42 +0100

    Update ip.py

--- ip.py
+++ ip.py
@@ -9,3 +9,7 @@ chat_id="xxxxxxxxxxxxxx"
 bot=telepot.Bot(TOKEN)
 bot.sendMessage(chat_id,ip )
 bot.sendMessage(chat_id,'https://whatismyipaddress.com/ip/' + ip)
+
+Print("thank you to have used my bot')
+
+bot.sendMessage(chat_id,'enjoy ;)')
